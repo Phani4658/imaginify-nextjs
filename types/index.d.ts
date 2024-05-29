4670d49ef9ebf889e44d29143167d0c55d20848d
Phani <officialphani.cse@gmail.com>
2024-05-29 19:42:35 +0530

    Add Pages

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,40 @@ declare type UpdateUserParams = {
     lastName: string | null;
     photo: string;
 }
+
+declare type SearchParamProps = {
+    params: { id: string; type: TransformationTypeKey };
+    searchParams: { [key: string]: string | string[] | undefined };
+};
+  
+
+declare type Transformations = {
+    restore?: boolean;
+    removeBackground?: boolean;
+    fillBackground?: boolean;
+    remove?: {
+        prompt: string,
+        removeShadow?: boolean,
+        multiple?: boolean
+    };
+    recolor?: {
+        prompt: string,
+        to: string,
+        multiple?: boolean
+    };
+}
+
+declare type TransformationType = {
+    type: string;
+    title: string;
+    subtitle: string;
+    config: Transformations;
+    icon: string;
+}
+
+declare type TransformationTypeKey =
+  | "restore"
+  | "fill"
+  | "remove"
+  | "recolor"
+  | "removeBackground";
