736ef6cbfda389790cae09617116898b67fc04d0
Phani <officialphani.cse@gmail.com>
2024-05-29 15:16:28 +0530

    Add DB and Models

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,48 @@
+import { transformationTypes } from "@/constants";
+import { auth } from "@clerk/nextjs/server";
+import { create } from "domain";
+import { Schema, model, models,  } from "mongoose";
+import { transform } from "next/dist/build/swc";
+
+
+export interface IImage extends Document {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: string;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author: {
+        _id: string;
+        firstName: string;
+        lastName: string;
+    };
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+    title: {type: String, required: true},
+    transformationType: {type: String, required: true},
+    publicId: {type: String, required: true},
+    secureUrl: {type: String, required: true},
+    width: {type: Number,},
+    height: {type: Number},
+    config: {type: Object},
+    transformationUrl: {type: URL},
+    aspectRatio: {type: String},
+    color: {type: String},
+    prompt: {type: String},
+    author: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+})  
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image
